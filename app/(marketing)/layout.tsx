6fde2cac1c08db898a80368852a8124fe116b0d3
abhijeetjohnkujur <abhijeetkujur000@gmail.com>
2024-05-20 00:17:04 +0530

    ADDED: Marketing page

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -1,9 +1,14 @@
+import { Navbar } from "../(marketing)/_components/Navbar"
+import { Footer } from "./_components/footer"
+
 const marketingLayout = ({children}:{children: React.ReactNode}) => {
     return (
         <div className="h-screen bg-slate-100">
+            <Navbar />
             <main className="pt-40 pb-20 bg-slate-100">
             {children}
             </main>
+            <Footer />
         </div>
     )
 }
---
+++ components/ui/Logo.tsx
@@ -0,0 +1,30 @@
+import Image from "next/image";
+import Link from "next/link";
+
+import { cn } from "@/lib/utils";
+import localFont from "next/font/local";
+
+const headingFont = localFont({
+    src: "../../public/fonts/font.woff2",
+})
+
+const Logo = () => {
+  return (
+    <>
+    <Link href="/">
+        <div className="hover:opacity-75 transition items-center gap-x-2 hidden md:flex">
+        <Image src="/logo.png"
+        alt="logo"
+        height={30}
+        width={30}
+        />
+        <p className={cn("text-lg text-neutral-700 pt-1", headingFont.className)}>
+            Taskmate
+        </p>
+        </div>
+    </Link>
+    </>
+  )
+}
+
+export default Logo;
