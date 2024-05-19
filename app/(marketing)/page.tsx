15f2cf20e606b3b85a9ac5b05b8669110e1338fe
abhijeetjohnkujur <abhijeetkujur000@gmail.com>
2024-05-19 12:31:11 +0530

    ADDED | MODIFIED: Fonts and Marketing page

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -2,10 +2,34 @@ import { Button } from "@/components/ui/button";
 import { Medal } from "lucide-react";
 import Link from "next/link";
 
+import localFont from "next/font/local"
+import { Poppins } from "next/font/google";
+
+import { cn } from "@/lib/utils";
+
+const headingFont = localFont({
+  src: "../../public/fonts/font.woff2",
+})
+
+const textFont = Poppins({
+  subsets: ["latin"],
+  weight:[
+    "100",
+    "200",
+    "300",
+    "400",
+    "500",
+    "600",
+    "700",
+    "800",
+    "900"
+  ]
+})
+
 const marketingpage = () => {
   return (
     <div className="flex items-center justify-center flex-col">
-        <div className="flex items-center justify-center flex-col">
+        <div className={cn("flex items-center justify-center flex-col", headingFont.className)}>
           <div className="mb-4 flex items-center border shadow-sm p-4 bg-amber-100 text-amber-700 rounded-full uppercase">
             <Medal className="h-6 w-6 mr-2" />
             No. 1 task management
@@ -16,7 +40,7 @@ const marketingpage = () => {
           </div>
         </div>
 
-        <div className="text-sm md:text-xl text-neutral-400 mt-4 max-w-xs md:max-w-2xl text-center mx-auto">
+        <div className={cn("text-sm md:text-xl text-neutral-400 mt-4 max-w-xs md:max-w-2xl text-center mx-auto",textFont.className)}>
           Collaborate, Manage project, and reach new productivity peaks. From high rises to the home office, the way your tea, works is unique - accomplishable.
         </div>
 
