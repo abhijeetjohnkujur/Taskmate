bc9f2328cb0f934c93d1447d031ab90ace0fa3ea
abhijeetjohnkujur <abhijeetkujur000@gmail.com>
2024-05-23 17:31:19 +0530

    ADDED | DELETED: Board, Boardlist, page

---
+++ app/(platform)/(dashboard)/organization/[organizationId]/_components/BoardList.tsx
@@ -0,0 +1,25 @@
+import { User2 } from "lucide-react";
+
+const BoardList = () => {
+  return (
+    <div className="space-y-4">
+        <div className="flex items-center font-semibold text-lg text-neutral-700">
+            <User2 className="h-6 w-6 mr-2" />
+            Your boards
+        </div>
+        <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4">
+            <div
+            role="button"
+            className="aspect-ratio relative h-full w-full bg-muted rounded-sm flex flex-col gap-y-1 items-center justify-center cursor-pointer hover:opacity-75 transition"
+            >
+                <p className="text-sm">Create new board</p>
+                <span className="text-xs">
+                    5 remaining
+                </span>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default BoardList;
--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,20 +1,15 @@
-import { db } from "@/lib/db"
-import Board from "./_components/Board"
-import Form from "./_components/Form"
+import { Separator } from "@/components/ui/separator"
+import {Info} from "./_components/Info"
+import BoardList from "./_components/BoardList"
+
 
 const organizationIdpage = async() => {
-
-    const boards = await db.board.findMany()
-
     return (
-        <div className="flex flex-col space-y-4">
-            <Form />
-            <div className="space-y-2">
-                {
-                    boards.map((board) => (
-                        <Board key={board.id} title={board.title} id={board.id} />
-                    ))
-                }
+        <div className="w-full mb-20">
+            <Info />
+            <Separator className="my-4" />
+            <div className="px-2 md:px-4">
+                <BoardList />
             </div>
         </div>
     )
