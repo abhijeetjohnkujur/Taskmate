338c456dd765519e04710e9ce165fd8cb1e0fabb
abhijeetjohnkujur <abhijeetkujur000@gmail.com>
2024-05-24 21:28:07 +0530

    ADDED: Suspense, Boardlist

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,6 +1,7 @@
 import { Separator } from "@/components/ui/separator"
 import {Info} from "./_components/Info"
-import BoardList from "./_components/BoardList"
+import {BoardList} from "./_components/BoardList"
+import { Suspense } from "react"
 
 
 const organizationIdpage = async() => {
@@ -9,7 +10,9 @@ const organizationIdpage = async() => {
             <Info />
             <Separator className="my-4" />
             <div className="px-2 md:px-4">
+                <Suspense fallback={<BoardList.Skeleton />}>
                 <BoardList />
+                </Suspense>
             </div>
         </div>
     )
