5423e4465052f089807717428f8354e2376ab990
abhijeetjohnkujur <abhijeetkujur000@gmail.com>
2024-05-29 12:54:21 +0530

    ADDED: Options, list utilities

--- app/(platform)/(dashboard)/board/[boardId]/_components/ListOptions.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/ListOptions.tsx
@@ -17,6 +17,7 @@ import { useAction } from "@/hooks/use-action";
 import { deleteList } from "@/actions/delete-list";
 import { toast } from "sonner";
 import { ElementRef, useRef } from "react";
+import { copyList } from "@/actions/copy-list";
 
 interface ListOptionsProps {
     data: List;
@@ -40,11 +41,28 @@ const ListOptions = ({
         }
     })
 
+    const {execute: executeCopy} = useAction(copyList,{
+        onSuccess: (data) => {
+            toast.success(`List "${data.title}" copied`)
+            closeRef.current?.click()
+        },
+        onError: (error) => {
+            toast.error(error)
+        }
+    })
+
     const onDelete = (formData: FormData) => {
         const id = formData.get("id") as string;
         const boardId = formData.get("boardId") as string;
 
         executeDelete({id,boardId})
+    }
+
+    const onCopy = (formData: FormData) => {
+        const id = formData.get("id") as string;
+        const boardId = formData.get("boardId") as string;
+
+        executeCopy({id,boardId})
     }
 
   return (
@@ -70,7 +88,7 @@ const ListOptions = ({
             >
                 Add card...
             </Button>
-            <form>
+            <form action={onCopy}>
                 <input hidden name="id" id="id" value={data.id}/>
                 <input hidden name="boardId" id="boardId" value={data.boardId}/>
                 <FormSubmit 
