cdd43032fa6630bc57b686bef649f67c96c67fd0
abhijeetjohnkujur <abhijeetkujur000@gmail.com>
2024-06-01 14:51:45 +0530

    ADDED: Card modal, provider modal, api fetch

--- app/(platform)/(dashboard)/board/[boardId]/_components/CardItem.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/CardItem.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useCardModal } from "@/hooks/use-card-modal";
 import { Draggable } from "@hello-pangea/dnd";
 import { Card } from "@prisma/client";
 
@@ -12,6 +13,9 @@ const CardItem = ({
   data,
   index
 }: CardItemProps) => {
+  
+  const cardModal = useCardModal()
+
   return (
     <Draggable draggableId={data.id} index={index}>
       {
@@ -20,6 +24,7 @@ const CardItem = ({
           { ...provided.draggableProps }
           { ...provided.dragHandleProps }
           ref={provided.innerRef}
+          onClick={() => cardModal.onOpen(data.id)}
             role="button"
             className="truncate border-2 border-transparent hover:border-black py-2 px-3 text-sm bg-white rounded-sm"
           >
