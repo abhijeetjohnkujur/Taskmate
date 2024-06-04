679cc8964dfb029ea4ad88e4bdae83bcdc2698a6
abhijeetjohnkujur <abhijeetkujur000@gmail.com>
2024-06-04 12:50:39 +0530

    ADDED: Subscription, Billing

---
+++ app/(platform)/(dashboard)/organization/[organizationId]/billing/page.tsx
@@ -0,0 +1,21 @@
+import { checkSubscription } from "@/lib/subscription";
+import { Info } from "../_components/Info";
+import { Separator } from "@/components/ui/separator";
+import { SubscriptionButton } from "./_components/SubscriptionButton";
+
+const Billingpage = async () => {
+
+    const isPro = await checkSubscription();
+
+  return (
+    <div className="w-full">
+        <Info isPro={isPro} />
+        <Separator className="my-2" />
+        <SubscriptionButton 
+        isPro={isPro}
+        />
+    </div>
+  )
+}
+
+export default Billingpage;
